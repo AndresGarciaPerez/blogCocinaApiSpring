760ca06269454add85fc47915aa79fec5d740195
AndresGarciaPerez <andygarcia041995@gmail.com>
2025-05-19 20:57:30 -0600

    se hicieron modificacion en la configuracion de las cors

--- src/main/java/myApp/blog/config/SecurityConfig.java
+++ src/main/java/myApp/blog/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -57,7 +58,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .cors(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable() ) //desactiva csrf para el API REST
+                .csrf(AbstractHttpConfigurer::disable) //desactiva csrf para el API REST
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/blogCocina/**").permitAll()
